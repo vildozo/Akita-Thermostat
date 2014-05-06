60ec605483484ab9364faca6196a7b89c9f5b1a5
Patricia Rosembluth Prado <patty_rosembluth@hotmail.com>
2014-05-06 02:01:24 -0400

    Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AkitaThermostat::Application.config.secret_key_base = '62f016de91a10b37a418d63162aa1468c181135498835437f3558c9b95c1289d5172ef03ec71b615c11d38fa214e704631d5ba10babb0ad1d4f53ff1e3b841f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AkitaThermostat::Application.config.session_store :cookie_store, key: '_akita_thermostat_session'
