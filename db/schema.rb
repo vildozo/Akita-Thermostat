57c8f0b5340fff8ba8ad0b74835205e2d7c0cb1d
Patricia Rosembluth Prado <patty_rosembluth@hotmail.com>
2014-05-13 14:14:15 -0400

    Cambio x

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -43,6 +42,11 @@ ActiveRecord::Schema.define(version: 20140513181951) do
   end
 
   add_index "thermostats", ["user_id"], name: "index_thermostats_on_user_id"
+
+  create_table "user_managers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
