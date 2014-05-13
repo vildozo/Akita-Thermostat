10d2a0df94bc29fe485c4a11c906aa5ca10445ed
Patricia Rosembluth Prado <patty_rosembluth@hotmail.com>
2014-05-13 12:20:01 -0400

    Vista usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 20140513152255) do
 
   add_index "thermostats", ["user_id"], name: "index_thermostats_on_user_id"
 
+  create_table "user_managers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -44,7 +49,6 @@ ActiveRecord::Schema.define(version: 20140513152255) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "rol"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
