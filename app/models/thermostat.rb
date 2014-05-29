c46fd241914dfceb570a0f9e7c3ce2e9ca23393a
Patricia Rosembluth Prado <patty_rosembluth@hotmail.com>
2014-05-29 10:47:17 -0400

    Validaciones en tiempo real

--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -3,7 +3,8 @@ class Thermostat < ActiveRecord::Base
   has_many :locations
   has_many :planners
 
-  validates_presence_of :serial, :message => "no puede ser vacio"
-  validates_length_of :serial, :minimum => 10, :too_short => "es muy corto (minimo 10 caracteres)"
-  validates_format_of :serial, :with => /\A[A-Z0-9]+\z/, :message => "solo acepta mayusculas y numeros"
+  validates :serial,uniqueness: {case_sensitive: false, :message => "Este termostato ya fue registrado"}	
+  validates :serial, presence: {:message => "Es un campo obligatorio"}
+  validates :serial, length: {minimum: 5, maximum: 10, :message => "El Detalle debe tener minimo 5 y maximo 10 caracteres"}
+  validates_format_of :serial, :with => /\A[A-Z0-9]+\z/, :message => "Solo ingresar letras mayusculas y numeros"
 end
