726d599019a1c4ca4581ec5f5fd3cfd797901110
Patricia Rosembluth Prado <patty_rosembluth@hotmail.com>
2014-06-03 09:21:10 -0400

    Mejora vista planners, correcion modelo

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -1,6 +1,6 @@
 class Planner < ActiveRecord::Base
   belongs_to :thermostat
 
-  validates :week_day, :max_temperature, :min_temperature, :schedule, presence: {:message => "Es un campo obligatorio"}
+  validates :week_day, :temp_max, :temp_min, :schedule, presence: {:message => "Es un campo obligatorio"}
   
 end
--- db/migrate/20140520163228_create_planners.rb
+++ db/migrate/20140520163228_create_planners.rb
@@ -2,8 +2,8 @@ class CreatePlanners < ActiveRecord::Migration
   def change
     create_table :planners do |t|
       t.string :week_day
-      t.integer :max_temperature
-      t.integer :min_temperature
+      t.integer :temp_max
+      t.integer :temp_min
       t.string :schedule
       t.references :thermostat, index: true
 
